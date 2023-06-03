76e7d47d3cb18afa9e7bca55486ccc24fc55dd7a
affiq23 <affiqem@gmail.com>
2023-06-03 12:25:09 -0500

    finished chapter 3

--- meadowlark.js
+++ meadowlark.js
@@ -1,39 +1,46 @@
-const express = require("express")
-const expressHandlebars = require("express-handlebars")
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express"); //calling the express function
+const expressHandlebars = require("express-handlebars") //calling engine function from express-handlebars
+const app = express(); //calling an instance of the function
+const port = process.env.port || 3000; //setting up port to use
 
-app.engine("handlebars", expressHandlebars({
-    defaultLayout: "main",
-}))
-app.set("view engine", "handlebars")
+//console.log('port: ' + port); //indicating port is set up
+app.engine('handlebars', expressHandlebars.engine({ defaultLayout: 'main' }));
+//app.engine and expressHandlebars.engine are two different engines
 
-//custom homepage
-app.get("/", (req, res) => {
-    res.type("text/plain") //specifies what type of text
-    res.send("Meadowlark Travel") //
-})
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + "/public")) //fixed the picture issue
 
-//custom about page
-app.get("/about", (req, res) => {
-    res.type("text/plain")
-    res.send("About Meadowlark Travel")
-})
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+const fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+]
+
+
+app.get('/about', (req, res) => {
+    const randomFortune = 
+    fortunes[Math.floor(Math.random()*fortunes.length)]
+    res.render('about', {fortune : randomFortune});
+});
 
 //needs to be below main pages, otherwise they won't work
 //custom 404 page
 app.use((req, res) => {
-    res.type("text/plain")
     res.status(404)
-    res.send("404 - Not Found")
+    res.render("404")
 })
 
 //custom 500 page
 app.use((err, req, res, next) => {
     console.error(err.message)
-    res.type("text/plain")
     res.status(500)
-    res.send("500 - Server Error")
+    res.render("500")
 })
 
 app.listen(port, () => console.log(`Express started on http://localhost:${port}; ` + `press Ctrl-C to terminate.`))
